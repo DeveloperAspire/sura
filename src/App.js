0eac80bd359985093064e23005e1309e8a8df8c2
Franklin <franklinokolie86@gmail.com>
2021-08-25 14:42:58 +0100

    Built the hero sectionand adjusted some styling on the Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Layout/Header'
+import Hero from './components/Layout/Hero'
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
      <Header/>
+     <Hero/>
       
     </React.Fragment>
   );
---
+++ src/components/Layout/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import classes from './Hero.module.css'
+import image from '../../images/illustration-working.svg'
+
+const Hero = () => {
+ return (
+   <main className={classes.hero}>
+     <div className={classes["hero--description"]}>
+       <h1>More than just shorter links</h1>
+       <p className={classes.text}>
+         Build your brand’s recognition and get detailed insights on how your
+         links are performing.
+       </p>
+       <p className={classes.button}>Get Started</p>
+     </div>
+     <img src={image} alt="" className={classes.img} />
+   </main>
+ );
+}
+
+export default Hero
