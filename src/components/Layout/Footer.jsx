71912ad592bc223b5e78e25ec5c9dae8f164a31c
Franklin <franklinokolie86@gmail.com>
2021-08-30 14:32:04 +0100

    Added the markup and styling for my trademark

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -9,31 +9,39 @@ import twitter from "../../images/icon-twitter.svg";
 const Footer = () => {
   return (
     <footer>
-      <h2>Sura</h2>
-      <ul className={classes.features}>
-        <li>Features</li>
-        <li> Link Shortening</li>
-        <li> Branded Links</li>
-        <li> Analytics</li>
-      </ul>
-      <ul>
-        <li> Resources</li>
-        <li>Blog</li>
-        <li>Developers</li>
-        <li>Support</li>
-      </ul>
-      <ul>
-        <li>Company</li>
-        <li> About</li>
-        <li>Our Team</li>
-        <li>Careers</li>
-        <li>Contact</li>
-      </ul>
-      <div>
-            <img src={facebook} alt="" />
-            <img src={twitter} alt="" />
-            <img src={pinterest} alt="" />
-            <img src={instagram} alt="" />
+      <div className={classes.container}>
+        <h2>Sura</h2>
+        <ul className={classes.features}>
+          <li>Features</li>
+          <li> Link Shortening</li>
+          <li> Branded Links</li>
+          <li> Analytics</li>
+        </ul>
+        <ul>
+          <li> Resources</li>
+          <li>Blog</li>
+          <li>Developers</li>
+          <li>Support</li>
+        </ul>
+        <ul>
+          <li>Company</li>
+          <li> About</li>
+          <li>Our Team</li>
+          <li>Careers</li>
+          <li>Contact</li>
+        </ul>
+        <div>
+          <img src={facebook} alt="" />
+          <img src={twitter} alt="" />
+          <img src={pinterest} alt="" />
+          <img src={instagram} alt="" />
+        </div>
+      </div>
+      <div className={classes.trademark}>
+        <p>
+          Made with ❤️ by{" "}
+          <a href="https://github.com/DeveloperAspire">Franklin Okolie</a>
+        </p>
       </div>
     </footer>
   );
